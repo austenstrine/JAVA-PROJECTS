b4954ea19d5dd2732f38e508194dcd3acd6e8475
austenstrine <austenstrine@gmail.com>
2017-06-13 13:51:36 -0600

    Java Final/Project for Employer
    I've been trying to think of ways to increase my value as an InfoWest
    employee, so I'm making a troubleshooting guide for new employees - I'm
    also making this my java Final. It's my first attempt at a real, 100%
    professional program that is actually useful.

---
+++ IWTroubleshooter/src/com/infowest/java/IWTroubleshooter.java
@@ -0,0 +1,70 @@
+/*
+ * https://docs.oracle.com/javase/tutorial/uiswing/components/tree.html
+ */
+
+package com.infowest.java;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JFrame;
+
+public class IWTroubleshooter extends JFrame implements ActionListener, ItemListener 
+{
+	
+	/*v***********
+	 * Variables *
+	 *************/
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6114354155618411165L;
+
+	/*v**************
+	 * Constructors *
+	 ****************/
+	
+	
+	
+	/*v*********
+	 * Methods *
+	 ***********/
+	
+	//getters
+	
+	
+	
+	//setters
+	
+	
+	
+	//other
+	
+	
+	
+	//overrides
+	
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*v*************
+	 * Main Method *
+	 ***************/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
